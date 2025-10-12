00c6cd9422b212367933c55fb88b63b94e46e92b
swensonmbailey <swensonmbailey@gmail.com>
2025-10-11 23:05:46 -0500

    week4

---
+++ coolpics/coolpics.js
@@ -0,0 +1,53 @@
+const menu = document.getElementById("navList");
+const menuButton = document.getElementById("menuButton");
+const gallery = document.querySelector('.gallery');
+const dialog = document.getElementById("dialog");
+const dialogImg = document.getElementById('dialogImg');
+const closeButton = document.getElementById("close-viewer");
+menuButton.addEventListener('click', () => {
+    hideElement(menu);
+});
+
+function hideElement(el) {
+    el.classList.toggle('hide');
+}
+function handleResize(){
+
+    if(window.innerWidth >= 1000){
+        hideElement(menu);
+        
+    }
+}
+function menuSetClosed() {
+    
+    if(window.innerWidth < 1000){
+        menu.classList.add('hide');
+    }
+}
+
+function openDialog(img) {
+    dialog.show();
+    let splitSrc = img.src.split('-');
+    dialogImg.src = splitSrc[0] + '-full.jpeg';
+
+}
+
+window.addEventListener('resize', handleResize);
+window.addEventListener('resize', menuSetClosed);
+
+window.addEventListener('load', handleResize);
+
+gallery.addEventListener('click', (event) => {
+    let img = event.target.closest('img');
+    openDialog(img);
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
+
+dialog.addEventListener('click', (event) => {
+  if (event.target !== dialogImg) {
+    dialog.close();
+  }
+})
