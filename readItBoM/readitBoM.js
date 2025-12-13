f9372379a385f551e6845114ebe3d58fd58af616
swensonmbailey <swensonmbailey@gmail.com>
2025-12-12 22:17:26 -0600

    project almost complete

---
+++ readItBoM/readitBoM.js
@@ -0,0 +1,55 @@
+videos = [
+    {title: "The Only Way to Prove Jesus is God: The Book of Mormon", type: "video", thumbnail: "images/The Book of Mormon.png", description: "Jesus is God. It's true, but how do we support this claim. How, then, can we safeguard our belief in Jesus Christ's full divinity? Answer: The Book of Mormon", link: "https://youtu.be/6Ik6B3MqE-A"},
+    {title: "Dr. Ryan Mullins on Divine Simplicity being Anti-Biblical", type: "video", thumbnail: "images/ClassicalTheismNotBiblical.png", description: "Dr. Ryan Mullins who promotes Neo-Classical Theism puts forth the reason why he believes Divine Simplicity, and classical theism in general, isn't biblical.", link: "https://youtu.be/12R2kz4qtyo"},
+    {title: "Pre-1832 Supporting Evidence for the Firsthand First Vision Accounts", type: "video", thumbnail: "images/firstVisionFirstHand.png", description: "Joseph Smith's First Vision was his first heavenly visitation in which he saw the Father and the Son. The first extant firsthand account of the vision appears in 1832. He later gave three other firsthand accounts of the vision. Critics try to say these accounts are conflicting or contradictory.  This video provides all firsthand accounts plus two less known evidences, both of which are pre-1832, that support the different Firsthand Accounts and show that the first vision wasn't made up later on.", link: "https://youtu.be/6DFW93M2vAI"},
+    {title: "A Latter-day Saint Interviews Ryan Mullins", type: "live", thumbnail: "images/ryanMullinsInterveiw.jpg", description: "A Latter-day Saint (mormon) sits down with Ryan Mullins and talks theology. Trinity, divine simplicity, classical theism, LDS theology, and whatever else comes to mind.", link: "https://youtube.com/live/U8IpKpigtW4"},
+    {title: "William Lane Craig's Bad Take on Mormons", type: "live", thumbnail: "images/WLCBadTake.jpg", description: "Let's see how William Lane Craig explains Mormons have a different Jesus. (Fun fact: he gets LDS theology wrong.) Let's also see how that definition would lead him to say that Catholics, Orthodox, and many other Protestant Faiths also have a different Jesus, but yet he doesn't voice that. Why the hypocrisy? ", link: "https://youtube.com/live/3f-TgL9-iKk"},
+    {title: "A Latter-day Saint Response to Classical Theism", type: "live", thumbnail: "images/refuttingClassical.png", description: "This is the ultimate guide to refuting classical theism. This video is my response to the recent 'Guide to Refuting Mormonism' video by  @SanctusTheology   in which he pulls the obligatory classical theist move, more or less claiming, 'If you don't believe in divine simplicity, you are theologically bankrupt.'", link: "https://youtube.com/live/zSnXHsm3Z4E"},
+    {title: "Why Philosophy Fails at God: LDS Revelation Wins", type: "collab", thumbnail: "images/throughthemist.jpg", description: "In this video, we examine why revelation—not human philosophy—is the central path to understanding God from an LDS perspective. We tackle key issues like the nature of God, apostasy, the Trinity, the role of modern prophets, and how the Book of Mormon serves as divine evidence.", link: "https://www.youtube.com/watch?v=dwavOtKs-R8"},
+    {title: "No! Latter-day Saints Don't Believe Jesus Is Created.", type: "video", thumbnail: "images/JesusNotCreated.png", description: "Creedal Christians claim that Latter-day Saints believe that Jesus Christ is a created being. This isn't true. LDS scripture and theology clearly teaches that Jesus is eternal and uncreated. In fact, LDS theology teaches that the 'mind of man' or better said, every person's spirit, is uncreated and eternal. Let's walk through the scriptural and theological proof together.", link: "https://youtu.be/sehjTmDsrds"},
+    {title: "Where was the trinity? Justin & Tertullian Didn't Teach It!", type: "video", thumbnail: "images/TrinityNotTaught.png", description: "Response to Trent Horn from  @TheCounselofTrent  on his attempts to use Justin Martyr and Tertullian as defenders of the trinity. Let's talk about how the trinity wasn't taught in the first 3 centuries, and actually refuted by the argumentation of Justin Martyr and Tertullian. ", link: "https://youtu.be/YZEPUeenArc"},
+    {title: "Elder Holland: The Book of Mormon as Evidence", type: "video", thumbnail: "images/elderHolland.png", description: "In his October 2025 General Conference talk Elder Holland taught that  the Book of Mormon can be evidence to us of the Restoration of the Gospel just as 'seeing' was an evidence of Jesus' power and mercy for the blind man. The Book of Mormon is a powerful witness of the Restoration of the Gospel of Jesus Christ. It has stood for 200 years, and whose evidence has only gotten stronger.", link: "https://youtu.be/Uh_woB34jMQ"},
+    {title: "Jay Dyer Runs From the Book of Mormon", type: "live", thumbnail: "images/JayDyer.png", description: "Let's look at a clip of an apparent 'W' for  @JayDyer   against a Mormon where Jay refuses to talk about the Book of Mormon or accept that the Book of Mormon is the founding miracle of the LDS church.  If Jay wants to 'own' Mormonism he will have to stop skirting around the Book of Mormon.", link: "https://youtube.com/live/NF2uv1ovHF0"},
+    {title: "Latter-days Saints Worship Christ: Why  @shamelesspopery  is wrong", type: "live", thumbnail: "images/WorshipJesus.jpg", description: "​ @shamelesspopery  recently made a video trying to prove that 'Mormons' don't worship Jesus. Throughout his video he basically equates worship with 'praying'. Let's talk about why he is wrong.", link: "https://youtube.com/live/OF_7zBwspk0"},
+    {title: "Alex O'Connor Lights Creedal Christianity on Fire With One Question", type: "collab", thumbnail: "images/alexOconnor.jpg", description: "Alex O'Connor had Cliffe and Stuart Knechtle on his podcast. He asked them a simple question that they simply  couldn't answer. This question has the potential to set mainstream creedal Christianity a blaze. Let's look at how this simple question is simply answered by the Restored Gospel of Jesus Christ.", link: "https://youtube.com/live/zeq9_wmDSkE"},
+    {title: "Response to @InspiringPhilosophy - The Christian Greek Philosophy Problem", type: "collab", thumbnail: "images/greekphilosophy.jpg", description: "Michael and Than from  @InspiringPhilosophy  made a response to Jacob from   @thoughtfulfaith2020 . Michael and Than try to claim Greek Philosophy didn't influence Christianity in a negative way. Let's talk about this.", link: "https://youtube.com/live/YeuH_hNvBC4"},
+    {title: "Oliver Cowdery's testimony during his excommunication: irrefutable", type: "video", thumbnail: "images/oliver.webp", description: "Oliver Cowdery was excommunicated from the church in 1838. However, as a letter he wrote during his excommunication proves, he never lost/denied his testimony. This testimony is irrefutable proof that he, Oliver, fully believed and promoted the reality of the visitations of both John the Baptist and Peter [, James, and John] to restore the priesthood.", link: "https://youtu.be/l8OyR9xXzI0"},
+    {title: "William L. Craig Agrees with Mormon Apologists: Greek Philosophy Misled Early Christian Fathers", type: "video", thumbnail: "images/wlcnicene.webp", description: "William Lane Craig, like the teachings of the Church of Jesus Christ of Latter-day saints,  doesn't believe in the historical understanding of the trinity. Why not? According to him, because Early Church Fathers were misled by accepting the middle platonic idea of the logos. He explains this influence known as Logos Christology was enshrined into the Nicene Creed. In other words, William Lane Craig agrees with LDS Apologists when we explain that Greek Philosophy, Platonism especially, negatively influenced early Christian Fathers and corrupted the Nicene Creed.", link: "https://youtu.be/jv6Gdvd9kBc"},
+    {title: "The Book of Abraham is Not the Nail in the Coffin of the Restored Gospel", type: "live", thumbnail: "images/bookofabraham.jpg", description: "Capturing Christianity just released yet another video on Mormonism. The ones he has previously released have been misses. This time he talks about the Book of Abraham. Let's talk about what he doesn't tell you about the Book of Abraham, and let's talk about why the Book of Abraham isn't the nail in the Coffin of the Restored Gospel (Mormonism)", link: "https://youtube.com/live/ZYubLtcOPUk"},
+    {title: "The Hypostatic Union Undermines Christ's Atonement. The Restored Gospel Doesn't.", type: "video", thumbnail: "images/hypostatic.webp", description: "I discuss with Sam from  @Throughthemist1830  how the Hypostatic Union (and other creeds) undermine and destroy Christ's Atonement. I then explain how the Restored Gospel and its rejection of the Hypostatic Union rescues the full power and meaning of Christ's sacrifice.", link: "https://youtu.be/X4KWVsv9EP4"},
+    {title: "Understanding Who Jesus Christ is | D&C Student Manual", type: "video", thumbnail: "images/whoisJesus.jpg", description: "What does the Doctrine and Covenants (a sacred, canonized scripture of the Church of Jesus Christ of Latter-day Saints) teach about Jesus Christ. What is His nature? What is His mission?", link: "https://youtu.be/HHPPFLqZErs"},
+    {title: "The Book of Mormon Does It Better: Jesus' Divinity", type: "collab", thumbnail: "images/Marvelous wonder collab.jpg", description: "Creedal Christians use their creeds and their tradition to support the belief that Jesus is God. But what if the creeds and their tradition have major flaws? What if they aren't scriptural? What if they aren't internally coherent?", link: "https://www.youtube.com/watch?v=Dg2bDLcfFEo"}
+
+];
+
+document.querySelectorAll('input[name="category"]').forEach(radio => {
+  radio.addEventListener('change', (e) => {
+    let value = e.target.value;
+    if(value == "all"){
+        displayContent(videos);
+    }else{
+        filteredVideos = videos.filter(vid => vid.type == value);
+        displayContent(filteredVideos)
+    }
+  });
+});
+
+function displayContent(videoList){
+    const videoContainer = document.getElementById("videoContainer");
+
+    videoContainer.innerHTML = ``;
+
+    videoList.forEach(vid =>{
+        const video = document.createElement("article");
+        video.innerHTML = `
+        <h3 class="video_title">${vid.title}</h3>
+        <a href="${vid.link}"><img class="thumbnail" src="${vid.thumbnail}" alt="Thumbail for: ${vid.title}"></a>
+        <p class="description">${vid.description}</p>
+        `;
+        videoContainer.appendChild(video);
+    });
+}
+
+window.addEventListener("load", ()=>{
+    displayContent(videos);
+});
